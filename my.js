a8b3362a32a943dffac512afac5479bd9857cfc2
dinhhieu2607 <dinhhieu26071990@gmail.com>
2021-06-02 18:28:28 +0700

    Laisuatnganhang

---
+++ my.js
@@ -0,0 +1,7 @@
+function tinhtien() {
+    let tien =+document.getElementById("sotien").value;
+    let month =+document.getElementById("month").value;
+    let laisuat =+document.getElementById("laisuat").value;
+    tong=tien*month*laisuat;
+    alert('so tien la 'tong);
+}
